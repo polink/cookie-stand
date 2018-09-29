9fdcee03dd023e4c86ab7b043d54ecba2338b636
Karl Polintan <kf.polintan@gmail.com>
2018-09-29 10:11:35 -0700

    Cleaned up the code & comments even more

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 'use strict';
-// next step: make function to make... store objects, likely an array that stores all the.... stores.
-// other next step: use document.getElementbyId() to put all this information into sales.html instead of the <table> in there.
-
 
 //Store constructor function
 var Store = function(name, minCust, maxCust, avgCook, cookSold){
@@ -32,33 +29,27 @@ Store.prototype.renderHours = function(){
   var storesContainer = document.getElementById('cookTable');
 
   // 2. new element // can see not appending to starting table.
-  //var headerEl = document.createElement('h2');
   var trEl = document.createElement('tr');
+
   // 3. Give element content
-  //  headerEl.textContent = this.name;
   var thEl = document.createElement('th'); //table header
   thEl.textContent = this.name;
   trEl.appendChild(thEl); //append table header to row
-  //append mincust to tds
-  for (var i in this.cookSold){
+
+
+  for (var i in this.cookSold){   //append minCust to tds
   var tdEl = document.createElement('td');
   tdEl.textContent = this.cookSold[i];
   trEl.appendChild(tdEl);
   
   //giving table ro td of avg# of purchased cookies per cust
 
-  // tdEl = document.createElement('td');
-  // tdEl.textContent = this.avgCook;
-  // trEl.appendChild(tdEl);
-  };
   // 4. append newly created element to container
   storesContainer.appendChild(trEl);
 
 };
-//=================
-
+//=======================
 // Function that renders all the stores
-//Good to put them in an array and just .push everything
 var allCookStores = [];
 var renderAllStores = function(){
   allCookStores.push(pikes.renderHours(),seaTac.renderHours(),seaCtr.renderHours(),alki.renderHours()); 
